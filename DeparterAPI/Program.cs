b849e74524dd5098ade24378f39f63063694592f
Estevam de Freitas Junior <estevamdefreitas37@gmail.com>
2022-09-16 19:55:30 -0300

    adicionado configuração dos services

--- DeparterAPI/Program.cs
+++ DeparterAPI/Program.cs
@@ -1,10 +1,43 @@
+using AutoMapper;
+using Persistence.Repositories.Implementation;
+using Persistence.Repositories.Interfaces;
+using Services.Services.Implementation;
+using Services.Services.Interfaces;
+using Services.Utilities;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 
+builder.Services.AddScoped<IServiceWrapper, ServiceWrapper>();
+builder.Services.AddScoped<IRepositoryWrapper, RepositoryWrapper>();
+
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: "AllowAll",
+                        policy =>
+                        {
+                            policy.AllowAnyOrigin();
+                            policy.AllowAnyMethod();
+                            policy.AllowAnyHeader();
+                        }
+                            );
+});
+
+var mapperConfig = new AutoMapper.MapperConfiguration(new MapperConfig());
+
+IMapper mapper = mapperConfig.CreateMapper();
+
+builder.Services.AddSingleton(mapper);
+
+AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+
 var app = builder.Build();
+
+app.UseCors("AllowAll");
 
 // Configure the HTTP request pipeline.
 
---
+++ Services/Services/Implementation/ServiceWrapper.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Persistence.Repositories.Interfaces;
+using Services.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Services.Implementation
+{
+    public class ServiceWrapper : IServiceWrapper
+    {
+        public ServiceWrapper(IRepositoryWrapper repository, IMapper mapper)
+        {
+
+        }
+    }
+}
