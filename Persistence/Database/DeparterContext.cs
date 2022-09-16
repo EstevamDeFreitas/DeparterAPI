27f98eb614fd36ca868e4eccd17c6835f5f6a520
Estevam de Freitas Junior <estevamdefreitas37@gmail.com>
2022-09-16 19:30:21 -0300

    Adicionar arquivos de projeto.

---
+++ Persistence/Database/DeparterContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.Database
+{
+    public class DeparterContext : DbContext
+    {
+        public DeparterContext(DbContextOptions<DeparterContext> options) : base(options)
+        {
+        }
+    }
+}
---
+++ Persistence/Repositories/Implementation/RepositoryWrapper.cs
@@ -0,0 +1,25 @@
+﻿using Persistence.Database;
+using Persistence.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.Repositories.Implementation
+{
+    public class RepositoryWrapper : IRepositoryWrapper
+    {
+        private DeparterContext _context;
+
+        public RepositoryWrapper(DeparterContext context)
+        {
+            _context = context;
+        }
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+    }
+}
