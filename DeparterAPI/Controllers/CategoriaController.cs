543a13b2e8a5dfbea374fbe9997cb8f005bcacc9
Estevam de Freitas Junior <estevamdefreitas37@gmail.com>
2022-10-09 11:07:53 -0300

    Adicionado endpoints de categoria

---
+++ DeparterAPI/Controllers/CategoriaController.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Services.DTO;
+using Services.Notations;
+using Services.Services.Interfaces;
+using Services.Utilities;
+
+namespace DeparterAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriaController : ControllerBase
+    {
+        protected readonly IServiceWrapper _serviceWrapper;
+
+        public CategoriaController(IServiceWrapper serviceWrapper)
+        {
+            _serviceWrapper = serviceWrapper;
+        }
+
+        [HttpPost]
+        [Authorize]
+        public IActionResult CreateCategoria([FromBody]CategoriaDTO categoria)
+        {
+            try
+            {
+                _serviceWrapper.CategoriaService.CreateCategoria(categoria);
+
+                return Ok(new Result<object>("Categoria Criada"));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new Result<object>(ex.Message));
+            }
+        }
+
+        [HttpGet]
+        [Authorize]
+        public IActionResult GetCategorias()
+        {
+            try
+            {
+                var categorias = _serviceWrapper.CategoriaService.GetCategorias();
+
+                return Ok(new Result<List<CategoriaDTO>>("Categorias Encontradas", categorias));
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(new Result<object>(ex.Message));
+            }
+        }
+
+        [HttpGet("{id}")]
+        [Authorize]
+        public IActionResult GetCategoria(Guid id)
+        {
+            try
+            {
+                var categoria = _serviceWrapper.CategoriaService.GetCategoria(id);
+
+                return Ok(new Result<CategoriaDTO>("Categoria Encontrada", categoria));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new Result<object>(ex.Message));
+            }
+        }
+
+        [HttpPut]
+        [Authorize]
+        public IActionResult UpdateCategoria([FromBody] CategoriaDTO categoria)
+        {
+            try
+            {
+                _serviceWrapper.CategoriaService.UpdateCategoria(categoria);
+
+                return Ok(new Result<object>("Categoria Atualizada"));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new Result<object>(ex.Message));
+            }
+        }
+
+        [HttpDelete("{id}")]
+        [Authorize]
+        public IActionResult DeleteCategoria(Guid id)
+        {
+            try
+            {
+                _serviceWrapper.CategoriaService.DeleteCategoria(id);
+
+                return Ok(new Result<object>("Categoria Deletada"));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new Result<object>(ex.Message));
+            }
+        }
+    }
+}
