533c300953d3cdee47a9d941a91df85f001183a9
Estevam de Freitas Junior <estevamdefreitas37@gmail.com>
2023-01-14 18:11:15 -0300

    Adicionado validação durante criação de email para evitar email duplicado

---
+++ Services/Exceptions/EmailJaEstaEmUso.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Exceptions
+{
+    [Serializable]
+    public class EmailJaEstaEmUso : Exception
+    {
+        public EmailJaEstaEmUso(string email):base($"O email {email} já está em uso") { }
+    }
+}
