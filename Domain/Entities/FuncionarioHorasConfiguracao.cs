bbe1f0497ae830ede0b6cd33b7e681797949e3dc
Estevam De Freitas <estevamdefreitas37@gmail.com>
2023-02-25 18:21:43 -0300

    Adicionada migracao das horas para o banco de dados

--- Domain/Entities/FuncionarioHorasConfiguracao.cs
+++ Domain/Entities/FuncionarioHorasConfiguracao.cs
@@ -1,15 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Domain.Entities
 {
+    [Table("funcionario_horas_configuracoes")]
     public class FuncionarioHorasConfiguracao : EntityBase
     {
+        [Required]
+        [Column("id_funcionario")]
         public Guid FuncionarioId { get; set; }
+        [Required]
+        [Column("tipo_configuracao")]
         public TipoConfigHora TipoConfiguracao { get; set; }
+        [Required]
+        [Column("minutos")]
         public int Minutos { get; set; }
 
         public Funcionario Funcionario { get; set; }
